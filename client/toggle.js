a9058fe3c9d3e41738e6b869cafa356b0d07f52d
1heart <yixin1996@gmail.com>
2015-09-26 17:01:39 -0400

    added vibrate

--- client/toggle.js
+++ client/toggle.js
@@ -13,6 +13,11 @@ Template.body.events({
 setDangerous = function() {
 	$("#notification-safe").hide();
 	$("#notification-dangerous").show();
+	
+	if (Meteor.isCordova) {
+	  navigator.vibrate([1000, 1000, 1000]);
+
+	}
 }
 
 setSafe = function() {
