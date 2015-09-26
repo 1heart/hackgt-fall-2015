1b1659989f52126e52a21419d316508edeea9fba
1heart <yixin1996@gmail.com>
2015-09-26 04:10:19 -0400

    wip frontend

--- hackgt-fall-2015.js
+++ hackgt-fall-2015.js
@@ -2,18 +2,48 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.body.helpers({
+    vices: function() {
+      return Vices.find({$or: [{owner: Meteor.userId()}]});
+    },
+    contactName: function() {
+      return "myName";
+    },
+    contactNumber: function() {
+      return "myNumber";
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.events({
+    "submit .new-vice": function(event) {
+      event.preventDefault();
+
+      // Get value from form element
+      var text = event.target.text.value;
+
+      // Insert a task into the collection
+      Vices.insert({
+        text: text,
+        createdAt: new Date(), // current time,
+        owner: Meteor.userId(),
+        username: Meteor.user().username
+      });
+
+      // Clear form
+      event.target.text.value = "";
     }
   });
+
+  Template.vice.events({
+    "click .delete": function() {
+      Vices.remove(this._id);
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
 }
 
 if (Meteor.isServer) {
@@ -21,3 +51,7 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+Vices = new Mongo.Collection("vices");
+
+
