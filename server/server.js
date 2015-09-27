b1e4addc54db9e605d00d76df860eac3b9b3c50f
1heart <yixin1996@gmail.com>
2015-09-27 00:58:17 -0400

    temp fix for yelp

--- server/server.js
+++ server/server.js
@@ -1,52 +1,52 @@
 
 
-Meteor.setInterval(function() {
-	// For every user
-	Locations.find({}).forEach(function(location){
-		latitude = location.latitude;
-		longitude = location.longitude;
-		id = location.owner;
-		// console.log(latitude);
-		// console.log(longitude);
-		// console.log(id);
-		v = Vices.find({owner: id});
-		// console.log(id);
-		v.forEach(function(vice){
-			searchTerms = processTerms(vice.text);
-			console.log(searchTerms);
-			allCurrent = Meteor.call("getResults", searchTerms, latitude, longitude).businesses;
-			pastLocs = vice.past;
-			currentLocs = closeCurrent(allCurrent, pastLocs);
-			// console.log(currentLocs);
-			Vices.update({$and: [{owner: id}, {text: vice.text}]},{
-				text: vice.text,
-				createdAt: vice.createdAt,
-				owner: vice.owner,
-				username: vice.username,
-				past: currentLocs
-			});
-		});
-		// console.log(v);
-		// For every vice belonging to the user
-		// var vices = Vices.find({owner: id});
-		// console.log(vices);
-		// Object.keys(vices).forEach(function(key){
-			// vice = vices[key];
-			// console.log(vice);
-			// searchTerms = processTerms(vice.text);
-			// console.log(searchTerms);
-			// allCurrent = Meteor.call("getResults",searchTerms, latitude, longitude).businesses;
-			// pastLocs = vice.past;
-			// currentLocs = closeCurrent(allCurrent, pastLocs);
-			// // update 'past' property in vice
-			// Vices.update({$and: [{owner: id}, {text: vice.text}]},{
-			// 	past: currentLocs
-			// })
-		// });
+// Meteor.setInterval(function() {
+// 	// For every user
+// 	Locations.find({}).forEach(function(location){
+// 		latitude = location.latitude;
+// 		longitude = location.longitude;
+// 		id = location.owner;
+// 		// console.log(latitude);
+// 		// console.log(longitude);
+// 		// console.log(id);
+// 		v = Vices.find({owner: id});
+// 		// console.log(id);
+// 		v.forEach(function(vice){
+// 			searchTerms = processTerms(vice.text);
+// 			console.log(searchTerms);
+// 			allCurrent = Meteor.call("getResults", searchTerms, latitude, longitude).businesses;
+// 			pastLocs = vice.past;
+// 			currentLocs = closeCurrent(allCurrent, pastLocs);
+// 			// console.log(currentLocs);
+// 			Vices.update({$and: [{owner: id}, {text: vice.text}]},{
+// 				text: vice.text,
+// 				createdAt: vice.createdAt,
+// 				owner: vice.owner,
+// 				username: vice.username,
+// 				past: currentLocs
+// 			});
+// 		});
+// 		// console.log(v);
+// 		// For every vice belonging to the user
+// 		// var vices = Vices.find({owner: id});
+// 		// console.log(vices);
+// 		// Object.keys(vices).forEach(function(key){
+// 			// vice = vices[key];
+// 			// console.log(vice);
+// 			// searchTerms = processTerms(vice.text);
+// 			// console.log(searchTerms);
+// 			// allCurrent = Meteor.call("getResults",searchTerms, latitude, longitude).businesses;
+// 			// pastLocs = vice.past;
+// 			// currentLocs = closeCurrent(allCurrent, pastLocs);
+// 			// // update 'past' property in vice
+// 			// Vices.update({$and: [{owner: id}, {text: vice.text}]},{
+// 			// 	past: currentLocs
+// 			// })
+// 		// });
 
-	});
+// 	});
 
-}, 5000);
+// }, 5000);
 
 // Replaces spaces with + signs
 var processTerms = function(term){
